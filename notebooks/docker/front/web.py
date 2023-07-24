5e5001c7f3640dcfc5205b7eb06ec5a18229838e
Roman Ababkov <zetpetneru@gmail.com>
2023-07-24 18:56:43 +0300

    fix n-skills

--- notebooks/docker/front/web.py
+++ notebooks/docker/front/web.py
@@ -33,7 +33,7 @@ assert np.all([skill_index_to_corrected[df_plot_polar['skill_id'][i]] ==
               df_plot_polar['Навык'][i] for i in skill_index_to_corrected.keys()])
 
 
-def plot_polar_graph(df: pd.DataFrame, prof_list: List[str], intersect_skills: bool = False, top_n: int = 30) -> go.Figure:
+def plot_polar_graph(df: pd.DataFrame, prof_list: List[str], top_n: int = 30) -> go.Figure:
     skill_set = []
 
     for prof_name in prof_list:
@@ -41,13 +41,9 @@ def plot_polar_graph(df: pd.DataFrame, prof_list: List[str], intersect_skills: b
         prof_skill = df_copy.sort_values(by=prof_name, ascending=False)[
             'Навык'][:top_n].values
 
-        if intersect_skills:
-            if len(skill_set) == 0:
-                skill_set = prof_skill
-            skill_set = [x for x in prof_skill if x in skill_set]
-        else:
-            skill_set = skill_set + \
-                [x for x in prof_skill if x not in skill_set]
+        skill_set = skill_set + [x for x in prof_skill if x not in skill_set]
+
+    skill_set = skill_set[:top_n]
 
     df_copy = df[df['Навык'].isin(skill_set)]
 
@@ -56,9 +52,12 @@ def plot_polar_graph(df: pd.DataFrame, prof_list: List[str], intersect_skills: b
     fig = go.Figure()
 
     for _, prof_name in enumerate(prof_list):
+        prof_sum = df_copy[prof_name].sum()
+
+        percentage_values = round((df_copy[prof_name] / prof_sum) * 100)
 
         fig.add_trace(go.Scatterpolar(
-            r=df_copy[prof_name],
+            r=percentage_values,
             theta=df_copy['Навык'],
             fill='toself',
             name=prof_name
@@ -67,7 +66,13 @@ def plot_polar_graph(df: pd.DataFrame, prof_list: List[str], intersect_skills: b
         fig.update_layout(
             polar=dict(
                 radialaxis=dict(
-                    visible=True
+                    visible=False,
+                    showline=False,
+                    showticklabels=False,
+                    gridcolor='rgba(0,0,0,0)'
+                ),
+                angularaxis=dict(
+                    gridcolor='rgba(0,0,0,0)'
                 )
             ),
             showlegend=True
@@ -78,10 +83,10 @@ def plot_polar_graph(df: pd.DataFrame, prof_list: List[str], intersect_skills: b
 
 def plot_skill_scatter(df: pd.DataFrame, width: int = 800, height: int = 600) -> go.Figure:
     color_list = [
-        '#F8A19F', '#AA0DFE', '#3283FE', '#1CBE4F', '#C4451C', '#F6222E', 
-        '#FE00FA', '#325A9B', '#FEAF16', 
+        '#F8A19F', '#AA0DFE', '#3283FE', '#1CBE4F', '#C4451C', '#F6222E',
+        '#FE00FA', '#325A9B', '#FEAF16',
         '#90AD1C', '#2ED9FF', '#B10DA1',
-         '#909090', '#FBE426',
+        '#909090', '#FBE426',
         '#FA0087', '#C075A6', '#FC1CBF'
     ]
 
@@ -89,8 +94,8 @@ def plot_skill_scatter(df: pd.DataFrame, width: int = 800, height: int = 600) ->
         'Data Scientist', 'ML инженер', 'Computer Vision', 'NLP',
         'Инженер данных', 'Big Data', 'Администратор баз данных', 'Аналитик данных',
         'Аналитик', 'Бизнес-аналитик', 'Продуктовый аналитик', 'Аналитик BI',
-        'Системный аналитик' ]
-    
+        'Системный аналитик']
+
     fig = px.scatter(df, x='x', y='y',
                      color='Профессия', hover_name='Навык',
                      hover_data={'x': False, 'y': False,
@@ -115,7 +120,7 @@ profession_list = [
 ]
 
 polar_fig = plot_polar_graph(
-    df_plot_polar, prof_list=profession_list, intersect_skills=True, top_n=25)
+    df_plot_polar, prof_list=profession_list, top_n=25)
 
 df_plot_scatter = pd.read_csv('data/scatter/best.csv')
 scatter_fig = plot_skill_scatter(df_plot_scatter, width=1000, height=600)
@@ -126,15 +131,16 @@ dropdownvalue_to_preset = {
     0: ['ML инженер', 'Data Scientist'],
     1: ['NLP', 'Computer Vision'],
     2: ['Аналитик', 'Аналитик данных', 'Системный аналитик', 'Продуктовый аналитик', 'Аналитик BI',
-                                                               'Бизнес-аналитик'],
-    3: ['Инженер данных','Администратор баз данных', 'Big Data']
+        'Бизнес-аналитик'],
+    3: ['Инженер данных', 'Администратор баз данных', 'Big Data']
 }
 
 
 app.layout = html.Div(
     children=[
-        html.H1('Добро пожаловать в SkillMap: Ваш путеводитель в мир навыков и профессий'),
-        html.Div(id='descr',style={'font-size': '0.9em'}, children=[
+        html.H1(
+            'Добро пожаловать в SkillMap: Ваш путеводитель в мир навыков и профессий'),
+        html.Div(id='descr', style={'font-size': '0.9em'}, children=[
             html.P("SkillMap - инновационный сервис, созданный специально для тех, кто хочет успешно развиваться в современном образовательном и профессиональном пространстве. Независимо от того, являетесь ли вы студентом, кандидатом, специалистом, работодателем, менеджером по персоналу, менеджером продукта или автором учебных курсов, SkillMap поможет вам осуществить вашу учебную и профессиональную стратегию наиболее эффективно"),
             html.P("Мы понимаем ваши потребности и проблемы. Вы стремитесь расширить свои навыки, понять, какие навыки наиболее востребованы и как их освоить. SkillMap предлагает вам уникальный инструмент для ориентирования в мире навыков и профессий."),
             html.P("При посещении нашей веб-страницы вы получите уникальную карту, которая наглядно демонстрирует наиболее востребованные навыки и их взаимосвязи. Вы сможете увидеть, какие навыки и специальности наиболее востребованы, и как они взаимосвязаны между собой. Наша визуализация интерактивная и позволяет вам исследовать различные пути и варианты развития. Мы уверены, что наш сервис поможет вам увидеть широкий спектр возможностей и принять взвешенные решения в отношении ваших навыков и карьеры."),
@@ -185,10 +191,11 @@ app.layout = html.Div(
                                 )
                             ]
                         ),
-                        html.Div(id='chart-container', style={'display': 'flex'})
+                        html.Div(id='chart-container',
+                                 style={'display': 'flex'})
                     ]
                 ),
-                
+
                 html.Div(
                     style={'flex': '1'},
                     children=[dcc.Graph(id='scatter-plot', figure=scatter_fig)]
@@ -196,31 +203,31 @@ app.layout = html.Div(
             ]
         )
 
-        
+
 
     ]
 )
 
 
-
 @app.callback(
     Output('chart-container', 'children'),
     Input('preset-dropdown', 'value'),
-    #Input('chart-checkboxes', 'value'),
+    # Input('chart-checkboxes', 'value'),
     Input('num-skills-slider', 'value')
 )
-def update_chart(selected_preset, 
-                 #selected_charts, 
+def update_chart(selected_preset,
+                 # selected_charts,
                  num_skills):
     charts = []
     if selected_preset:
         prof_list = dropdownvalue_to_preset[selected_preset]
     else:
         prof_list = dropdownvalue_to_preset[0]
-    
+
     polar_fig = plot_polar_graph(
-        df_plot_polar, prof_list=prof_list, intersect_skills=True, top_n=num_skills)
-    charts.append(dcc.Graph(id='polar-plot', figure=polar_fig, style={'width': '90vh'}))
+        df_plot_polar, prof_list=prof_list, top_n=num_skills)
+    charts.append(dcc.Graph(id='polar-plot',
+                  figure=polar_fig, style={'width': '90vh'}))
 
     return charts
 
